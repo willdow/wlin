d8459685818585ba667bbab9c0b3e066821c71dd
willyan <willyanlin@gmail.com>
2022-05-24 22:48:47 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  // app
+  app: {
+    head: {
+      title: 'Willyan LIN',
+      titleTemplate: '%s - Blog',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'Willyan LIN',
+          name: 'Willyan LIN',
+          content: 'Blog',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        }
+      }
+    },
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<div
+		class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto space-y-2"
+	>
+		<h1 class="text-2xl">About</h1>
+		<p>Hey there</p>
+	</div>
+</template>
