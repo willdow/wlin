73c80d3b092d14e1c683b2ac5c383b44169854b3
Willyan Lin <willyanlin@gmail.com>
2022-06-06 18:41:43 +0200

    feat: 🦄 tools page ok

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,48 @@
     <div class="mt-8">
       This site is made by Nuxt3 + Vue3 Composition API + Tailwind CSS
     </div>
+    <div class="mt-16">Take a look at :</div>
+    <div class="sm:flex-row flex-col flex sm:items-baseline">
+      <div
+        class="m-2 border border-gray-300 px-6 py-5 flex items-center justify-center hover:border-gray-400"
+      >
+        <NuxtLink to="/tips/">
+          <h1
+            class="text-sm font-medium underline decoration-2 underline-offset-2"
+          >
+            Tips
+          </h1>
+          <h2 class="text-sm text-gray-400 truncate pt-2">
+            Some tips that i want to share.
+          </h2>
+        </NuxtLink>
+      </div>
+      <div
+        class="m-2 border border-gray-300 px-6 py-5 flex items-center justify-center hover:border-gray-400"
+      >
+        <NuxtLink to="/tools/">
+          <h1
+            class="text-sm font-medium underline decoration-2 underline-offset-2"
+          >
+            Tools
+          </h1>
+          <h2 class="text-sm text-gray-400 truncate pt-2">
+            The tools I use on a daily basis.
+          </h2>
+        </NuxtLink>
+      </div>
+      <div
+        class="m-2 border border-gray-300 px-6 py-5 flex items-center justify-center hover:border-gray-400"
+      >
+        <NuxtLink to="/projects/">
+          <h1
+            class="text-sm font-medium underline decoration-2 underline-offset-2"
+          >
+            Project
+          </h1>
+          <h2 class="text-sm text-gray-400 truncate pt-2">Project developed</h2>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
